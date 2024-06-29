b134eaf8da80e11cb5948f8e685fd8131533d986
Bartosz Król <barto.kro@gmail.com>
2024-06-30 00:44:44 +0200

    7-improve layout component, add navigation component

---
+++ src/layout/components/navigation/Navigation.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './navigation.module.scss';
+
+export const Navigation = () => {
+    console.log('nav');
+
+    return (
+        <div className={styles.nav}>
+            {'Nav'}
+        </div>
+    );
+};
