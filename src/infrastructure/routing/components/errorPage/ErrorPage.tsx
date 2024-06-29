e85990045d169159a88300ef67a119a7af0298c4
Bartosz Król <barto.kro@gmail.com>
2024-06-30 00:04:05 +0200

    7-simplify errorPage structure, remove unneeded component

--- src/infrastructure/routing/components/errorPage/ErrorPage.tsx
+++ src/infrastructure/routing/components/errorPage/ErrorPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './errorPage.module.scss';
-import { ErrorStructure } from './components/errorStructure/ErrorStructure';
+import { Link } from 'react-router-dom';
 
 export const ErrorPage = () => (
     <div className={styles.container}>
-        <ErrorStructure statusText={'Something went wrong...'} />
+        <h1 className={styles.oops}>{'Ooops!!!'}</h1>
+        <p className={styles.text}>{`The page you are looking for doesn't exist`}</p>
+        <Link className={styles.link} to={`/`}>{'Return to Homepage'}</Link>
     </div>
 );
