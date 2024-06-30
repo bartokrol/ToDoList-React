a8642f439f4bdbd4f94390d39a537cedcb7106bc
Bartosz Król <barto.kro@gmail.com>
2024-06-30 15:47:26 +0200

    9-create burger component

--- src/layout/components/navigation/Navigation.tsx
+++ src/layout/components/navigation/Navigation.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './navigation.module.scss';
+import { Burger } from '../burger/Burger';
 
-export const Navigation = () => (
-    <div className={styles.nav}>
-        <div className={styles.title}>
-            {'ToDoList '}
-            <p className={styles.author}>{'by Bartosz Król'}</p>
+export const Navigation = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleBurgerClick = () => {
+        setIsOpen(prev => !prev);
+    };
+
+    return (
+        <div className={styles.nav}>
+            <div className={styles.title}>
+                {'ToDoList '}
+                <p className={styles.author}>{'by Bartosz Król'}</p>
+            </div>
+            <Burger clickAction={handleBurgerClick} />
         </div>
-        <div>{'burger'}</div>
-    </div>
-);
+    );
+};
