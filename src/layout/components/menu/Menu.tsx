d5bc59a8a6fe3df83ed00edb3c16c36f292c9016
Bartosz Król <barto.kro@gmail.com>
2024-07-01 18:41:09 +0200

    9-expand menu component with todolist link, change icons imports

--- src/layout/components/menu/Menu.tsx
+++ src/layout/components/menu/Menu.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { ROUTES_PATHS } from '../../../infrastructure/routing/constants';
 import React from 'react';
 import styles from './menu.module.scss';
@@ -8,10 +8,11 @@ export const Menu = () => (
     <div className={styles.container}>
         <div className={styles.links}>
             {MENU_LINKS.map(link => (
-                <Link className={styles.linkWrapper} to={link.href}>
+                <NavLink className={({ isActive }) => `${styles.linkWrapper} ${isActive ? styles.activeLink : ''}`}
+                    to={link.href}>
                     <img className={styles.img} src={link.svg} alt={link.alt} />
                     <p className={styles.linkText}>{link.text}</p>
-                </Link>
+                </NavLink>
             ))}
         </div>
 
--- src/layout/components/menu/constants.ts
+++ src/layout/components/menu/constants.ts
@@ -1,32 +1,39 @@
 import { ROUTES_PATHS } from '../../../infrastructure/routing/constants';
-import loginLogo from '../../../public/svgs/login_logo.svg';
-import registrationLogo from '../../../public/svgs/registration_logo.svg';
-import settingsLogo from '../../../public/svgs/settings_logo.svg';
-import historyLogo from '../../../public/svgs/history_logo.svg';
+import loginIcon from '../../../public/svgs/login_icon.svg';
+import registrationIcon from '../../../public/svgs/registration_icon.svg';
+import settingsIcon from '../../../public/svgs/settings_icon.svg';
+import historyIcon from '../../../public/svgs/history_icon.svg';
+import todoIcon from '../../../public/svgs/todo_icon.svg';
 
 export const MENU_LINKS = [
     {
+        href: ROUTES_PATHS.toDoListPage,
+        text: 'todolist',
+        svg: todoIcon,
+        alt: 'ToDoList Image',
+    },
+    {
         href: ROUTES_PATHS.loginPage,
         text: 'login',
-        svg: loginLogo,
+        svg: loginIcon,
         alt: 'Login Image',
     },
     {
         href: ROUTES_PATHS.registrationPage,
         text: 'registration',
-        svg: registrationLogo,
+        svg: registrationIcon,
         alt: 'Registration Image',
     },
     {
         href: ROUTES_PATHS.historyPage,
         text: 'history',
-        svg: historyLogo,
+        svg: historyIcon,
         alt: 'History Image',
     },
     {
         href: ROUTES_PATHS.settingsPage,
         text: 'settings',
-        svg: settingsLogo,
+        svg: settingsIcon,
         alt: 'Settings Image',
     },
 ];
